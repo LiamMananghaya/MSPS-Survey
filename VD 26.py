51a9ed23ad76e2bb8cd6355bd2394d6e0a67d54b
LiamMananghaya <lgjuly2006@gmail.com>
2026-02-08 00:30:35 -0600

    Add files via upload

---
+++ VD 26.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Annual MBAGM Survey")
+
+favSpecies = st.selectbox(
+    "What is your favorite species of penguins?",
+    ["Emperor Penguin", "King Penguin", "Macaroni Penguin", "Little Penguin", "Other"]
+)
+
+if favSpecies == "Other":
+    otherSpecies = st.text_input("Please specify:")
+
+if st.button("Submit"):
+    if favSpecies == "Other":
+        finalSpecies = otherSpecies
+    else:
+        finalSpecies = favSpecies
+
