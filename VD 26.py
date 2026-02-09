12f5fea25f7dd7b13ad8ecb9cd4cf293305f8e9d
LiamMananghaya <lgjuly2006@gmail.com>
2026-02-08 23:05:00 -0600

    Update VD 26.py

--- VD 26.py
+++ VD 26.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Annual MBAGM Survey")
+st.title("Annual UMMBGB Survey")
 
 if "step" not in st.session_state: 
     st.session_state.step = 1 # initializes to start page count
@@ -14,13 +14,30 @@ if st.session_state.step == 1:
     if favSpecies == "Other":
         otherSpecies = st.text_input("Please specify:")
 
-    if st.button("Submit"):
+    if st.button("Next"):
         if favSpecies == "Other":
             finalSpecies = otherSpecies
         else:
             finalSpecies = favSpecies
+        
+        st.session_state.favSpecies = favSpecies
+        st.session_state.step = 2
+        st.rerun()
 
-birds = st.selectbox(
-    "Are penguins birds?",
-    ["Yes", "No", "Not sure"]
-)
+# Q2
+elif st.session_state.step == 2:
+    location = st.text_input(
+        "Where do you think your favorite species of penguins are commonly located?",
+        ["Yes", "No", "Not sure"]
+    )
+
+    if st.button("Next"):
+        st.session_state.location = location
+        st.session_state.step = 3
+        st.rerun
+
+# Final page
+elif st.session_state.step == 3:
+    st.success("Survey Complete! On behalf of the University of Manitoba's Most Beautiful Girl's Boyfriend (UMMBGB), we thanks for expressing an interest in learning about penguins! Here is a summary of your answers:")
+    st.write("Species:", st.session_state.species)
+    st.write("Location:", st.session_state.location)
