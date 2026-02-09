a48e37fb40ef8e1c1556957cd7fdb56f9d3c9055
LiamMananghaya <lgjuly2006@gmail.com>
2026-02-08 22:53:23 -0600

    Update VD 26.py

--- VD 26.py
+++ VD 26.py
@@ -2,19 +2,23 @@ import streamlit as st
 
 st.title("Annual MBAGM Survey")
 
-favSpecies = st.selectbox(
-    "What is your favorite species of penguins?",
-    ["Emperor Penguin", "King Penguin", "Macaroni Penguin", "Little Penguin", "Other"]
-)
+if "step" not in st.session_state: 
+    st.session_state.step = 1 # initializes to start page count
 
-if favSpecies == "Other":
-    otherSpecies = st.text_input("Please specify:")
+if st.session_state.step == 1:
+    favSpecies = st.selectbox(
+        "What is your favorite species of penguins?",
+        ["Emperor Penguin", "King Penguin", "Macaroni Penguin", "Little Penguin", "Other"]
+    )
 
-if st.button("Submit"):
     if favSpecies == "Other":
-        finalSpecies = otherSpecies
-    else:
-        finalSpecies = favSpecies
+        otherSpecies = st.text_input("Please specify:")
+
+    if st.button("Submit"):
+        if favSpecies == "Other":
+            finalSpecies = otherSpecies
+        else:
+            finalSpecies = favSpecies
 
 birds = st.selectbox(
     "Are penguins birds?",
